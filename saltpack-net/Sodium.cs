de40e9febcf8281e92babd1185ab9ffee626ba2d
Brian Friesen <brianfriesen@quickenloans.com>
2017-10-03 11:24:02 -0400

    Add Sodium class
    Invokes sodium_init in the static constructor, otherwise empty. Will add
    libsodium methods as necessary.

---
+++ saltpack-net/Sodium.cs
@@ -0,0 +1,29 @@
+﻿using RockLib.Interop;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Saltpack
+{
+    internal static class Sodium
+    {
+        private static readonly EmbeddedNativeLibrary _sodiumNative;
+
+        static Sodium()
+        {
+            _sodiumNative = new EmbeddedNativeLibrary(
+                "libsodium",
+                new DllInfo(TargetRuntime.Win32, "Saltpack.win_x86.native.libsodium.dll", "Saltpack.win_x86.native.msvcr120.dll"),
+                new DllInfo(TargetRuntime.Win64, "Saltpack.win_x64.native.libsodium.dll", "Saltpack.win_x64.native.msvcr120.dll"),
+                new DllInfo(TargetRuntime.Mac, "Saltpack.osx_x64.native.libsodium.dylib"),
+                new DllInfo(TargetRuntime.Linux, "Saltpack.linux_x64.native.libsodium.so"));
+
+            if (sodium_init() == -1)
+                throw new InvalidOperationException("The libsodium library failed to initialize: sodium_init() returned -1.");
+        }
+
+        private static int sodium_init() => _sodiumNative.GetDelegate<sodium_init_>(nameof(sodium_init)).Invoke();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int sodium_init_();
+    }
+}
