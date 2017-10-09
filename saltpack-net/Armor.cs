bdbf6a857827f3b5303ebd7b431752f34f5e7a84
Brian Friesen <brianfriesen@quickenloans.com>
2017-10-09 09:15:48 -0400

    Change namespace to SaltpackDotNet
    Having the namespace the same as the class makes it cumbersome to use.

--- saltpack-net/Armor.cs
+++ saltpack-net/Armor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace Saltpack
+namespace SaltpackDotNet
 {
     public static partial class Saltpack
     {
--- saltpack-net/Sodium.cs
+++ saltpack-net/Sodium.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace Saltpack
+namespace SaltpackDotNet
 {
     internal static class Sodium
     {
